d53f89a51070d88c71955f4dabdb2b6dfea8bd9d
Vlad6555z <Vlad6555z@gmail.com>
2020-01-11 11:08:52 +0200

    Signed-off-by: Vlad6555z <Vlad6555z@gmail.com>

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,7 @@
+import "/svg4everybody/dist/svg4everybody.min.js";
+import "/bootstrap/dist/js/bootstrap.min.js";
+import "/wow.js/dist/wow.min.js";
+
 function openNav() {
   document.querySelector(".myNav").style.width = "100%";
 }
--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,13 +1,7 @@
 module.exports = function() {
   $.gulp.task("libsJS:dev", () => {
-    var libs = [
-      "node_modules/svg4everybody/dist/svg4everybody.min.js",
-      "node_modules/bootstrap/dist/js/bootstrap.min.js",
-      "node_modules/wow.js/dist/wow.min.js"
-    ];
     return $.gulp
-      .src(libs)
-      .pipe($.gp.concat("libs.min.js"))
+      .src("./dev/static/js/main.js")
       .pipe($.gulpWebpack($.webpackConfig, $.webpack))
       .pipe($.gulp.dest("./build/static/js/"))
       .pipe(
@@ -18,15 +12,9 @@ module.exports = function() {
   });
 
   $.gulp.task("libsJS:build", () => {
-    var libs = [
-      "node_modules/svg4everybody/dist/svg4everybody.min.js",
-      "node_modules/bootstrap/dist/js/bootstrap.min.js",
-      "node_modules/wow.js/dist/wow.min.js"
-    ];
     return $.gulp
-      .src(libs)
-      .pipe($.gp.concat("libs.min.js"))
-      .pipe($.gulpWebpack(webpackConfig, webpack))
+      .src("./dev/static/js/main.js")
+      .pipe($.gulpWebpack($.webpackConfig, $.webpack))
       .pipe($.gulp.dest("./build/static/js/"));
   });
 
@@ -34,7 +22,7 @@ module.exports = function() {
     return $.gulp
       .src(["./dev/static/js/*.js", "!./dev/static/js/libs.min.js"])
       .pipe($.gulp.dest("./build/static/js/"))
-      .pipe($.gulpWebpack(webpackConfig, webpack)) //del
+      .pipe($.gulpWebpack($.webpackConfig, $.webpack)) //del
       .pipe(
         $.browserSync.reload({
           stream: true
